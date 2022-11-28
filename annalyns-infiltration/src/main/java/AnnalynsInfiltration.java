724deec7b537001f5b9cda72df9a908bcf55c779
Rafael Santos <rafaelsantosdearaujo@gmail.com>
2022-11-28 18:44:38 -0300

    annalyns-infiltration feito

---
+++ annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -0,0 +1,19 @@
+class AnnalynsInfiltration {
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        return !knightIsAwake;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        return archerIsAwake || prisonerIsAwake || knightIsAwake ;
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        return !archerIsAwake && prisonerIsAwake;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        return  (!knightIsAwake && !archerIsAwake && prisonerIsAwake) || (!archerIsAwake && petDogIsPresent);
+
+
+    }
+}
